3112d24be671792dfe573dfcb8026b574c5c0035
mizutanikirin <mizutani@thedesignium.com>
2020-10-21 16:33:36 +0900

    AzureKinectのJoint等Enumを追加

--- KirinUtil/Assets/KirinUtil/Scripts/KRNEnum.cs
+++ KirinUtil/Assets/KirinUtil/Scripts/KRNEnum.cs
@@ -1,12 +1,15 @@
 ﻿
-namespace KirinUtil {
+namespace KirinUtil
+{
     public enum ImageFormat { PNG, JPG };
 
-    public enum SpecialFolder {
+    public enum SpecialFolder
+    {
         MyDocuments, Desktop, ProgramFiles, Startup, System
     };
 
-    public enum KinectJoint {
+    public enum KinectJoint
+    {
         SpineBase = 0,
         SpineMid = 1,
         Neck = 2,
@@ -34,15 +37,60 @@ namespace KirinUtil {
         ThumbRight = 24
     }
 
-    public enum Direction {
+    public enum AzureJoint : int
+    {
+        Pelvis = 0,
+        SpineNaval = 1,
+        SpineChest = 2,
+        Neck = 3,
+        Head = 4,
+
+        ClavicleLeft = 5,
+        ShoulderLeft = 6,
+        ElbowLeft = 7,
+        WristLeft = 8,
+        HandLeft = 9,
+
+        ClavicleRight = 10,
+        ShoulderRight = 11,
+        ElbowRight = 12,
+        WristRight = 13,
+        HandRight = 14,
+
+        HipLeft = 15,
+        KneeLeft = 16,
+        AnkleLeft = 17,
+        FootLeft = 18,
+
+        HipRight = 19,
+        KneeRight = 20,
+        AnkleRight = 21,
+        FootRight = 22,
+
+        Nose = 23,
+        EyeLeft = 24,
+        EarLeft = 25,
+        EyeRight = 26,
+        EarRight = 27,
+
+        HandtipLeft = 28,
+        ThumbLeft = 29,
+        HandtipRight = 30,
+        ThumbRight = 31
+    }
+
+    public enum Direction
+    {
         Up, Down, Left, Right
     }
 
-    public enum FadeType {
+    public enum FadeType
+    {
         FadeIn, FadeOut
     }
 
-    public enum Month {
+    public enum Month
+    {
         January = 1,
         February = 2,
         March = 3,
@@ -57,7 +105,13 @@ namespace KirinUtil {
         December = 12
     }
 
-    public enum AxisType {
+    public enum AnimationType
+    {
+        Horizon, Vertical, Random, Shuffle, Rotate, Scale
+    }
+
+    public enum AxisType
+    {
         X, Y, Z, XY, XZ, YZ, XYZ
     }
 }
