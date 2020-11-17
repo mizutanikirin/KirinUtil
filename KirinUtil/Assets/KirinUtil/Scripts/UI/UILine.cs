e2a7437194d4fbe765a0b2562fcf9bba76ff8d0a
mizutanikirin <mizutani@thedesignium.com>
2020-11-17 15:34:30 +0900

    UILineで指定通りに線が引けないバグを修正

--- KirinUtil/Assets/KirinUtil/Scripts/UI/UILine.cs
+++ KirinUtil/Assets/KirinUtil/Scripts/UI/UILine.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-namespace KirinUtil {
+namespace KirinUtil
+{
 
     [RequireComponent(typeof(Image))]
-    public class UILine : MonoBehaviour {
+    public class UILine : MonoBehaviour
+    {
         //----------------------------------
         //  線を引く(gui)
         //----------------------------------
@@ -15,19 +17,20 @@ namespace KirinUtil {
         public Vector2 endPos;
         public float thick;
 
-        void Start() {
+        public void Draw()
+        {
 
             // 長さ
             float lineLength = Vector2.Distance(startPos, endPos);
             gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(thick, lineLength);
 
             // 角度
-            float lineAngle = Angle2(endPos, startPos) + 90f;
+            float lineAngle = Angle2(startPos, endPos) + 90f;
             gameObject.transform.localRotation = Quaternion.Euler(0, 0, lineAngle);
 
             // 再度位置を調整
-            float moveX = lineLength * 0.5f * Mathf.Cos(lineAngle * Mathf.Deg2Rad);
-            float moveY = lineLength * 0.5f * Mathf.Sin(lineAngle * Mathf.Deg2Rad);
+            float moveY = lineLength * 0.5f * Mathf.Cos(lineAngle * Mathf.Deg2Rad);
+            float moveX = lineLength * 0.5f * Mathf.Sin(lineAngle * Mathf.Deg2Rad);
             gameObject.transform.localPosition = new Vector3(
                 startPos.x + moveX,
                 startPos.y - moveY,
@@ -35,7 +38,8 @@ namespace KirinUtil {
             );
         }
 
-        private static float Angle2(Vector2 p1, Vector2 p2) {
+        private float Angle2(Vector2 p1, Vector2 p2)
+        {
             float dx = p2.x - p1.x;
             float dy = p2.y - p1.y;
             float rad = Mathf.Atan2(dy, dx);
